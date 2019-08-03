ae3884219b7fdb7c8eb7bc99cdff3307fdc28681
priyankasagi1991 <svpriyanka1991@gmail.com>
2019-08-03 10:53:28 -0400

    Updated the files

--- QuickDBD-Employee_db ERD_logical_schemata.sql
+++ QuickDBD-Employee_db ERD_logical_schemata.sql
@@ -69,3 +69,9 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
